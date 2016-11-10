722f088f6dc8b64aa1b059a61e218f68ff532afe
Jesse L Lawson <homestarflyer@gmail.com>
2016-11-09 22:11:06 -0500

    show and index page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,10 @@
 class PicsController < ApplicationController
-
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
   def index
+    @pics = Pics.all
+  end
+
+  def show
   end
 
   def new
@@ -17,11 +21,12 @@ class PicsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   private
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
 end

eac0a8dc72955735927bf69fec1698348ea563f5
Jesse L Lawson <homestarflyer@gmail.com>
2016-11-16 12:57:36 -0500

    add acts_as_votable gem and set up

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,9 @@
 class Pic < ActiveRecord::Base
+  acts_as_votable
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default-burning-house.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 end

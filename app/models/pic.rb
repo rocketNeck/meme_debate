279a3093d57e1f99149c9a808cd6734ef4af0a3c
Jesse L Lawson <homestarflyer@gmail.com>
2016-11-16 12:22:36 -0500

    paperclip gem and uploading images

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,3 +1,6 @@
-class Pic < ApplicationRecord
+class Pic < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default-burning-house.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
